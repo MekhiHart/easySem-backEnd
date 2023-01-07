cb75836227572e3fb6c0825cf981e40a62ccb78f
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-07 15:53:04 -0800

    Created an API that collects all college majors, and remove duplicated college majors

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 const express = require("express")
+const puppeteer = require("puppeteer")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const url = "https://learnwebcode.github.io/practice-requests/"
+
+const fs = require("fs/promises")
 
 
-app.get("/api", (req,res) =>{
+/* 
+HTTP requests
+fetch - gets data from server to client
+post - adds data to server from client
+
+*/
+
+async function start(){
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({path:"amazing.png"})
+  await browser.close()
+}
+
+
+app.get("/api", (req,res) =>{ // ! This is an API!
     res.json({message:"Hello from server"})
 })
+
+app.get('/screenshot', async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url); // URL is given by the "user" (your client-side application)
+  const screenshotBuffer = await page.screenshot();
+
+  // Respond with the image
+  res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshotBuffer.length
+  });
+  res.end(screenshotBuffer);
+
+  await browser.close();
+})
+
+app.get("/classes", async (req,res) =>{
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const url = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2023/By_Subject/ACCT.html"
+  await page.goto(url)
+
+  const names = await page.evaluate(() =>{
+    return Array.from(document.querySelectorAll(".courseHeader h4")).map(name => name.textContent)
+  })
+  
+  res.json({message:names})
+
+  await browser.close()
+})
+
+
+app.get("/collegeMajors", async (req,res) =>{
+  const browser  = await puppeteer.launch()
+  const page = await browser.newPage()
+  const url = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2023/By_College/index.html"
+  await page.goto(url)
+
+  let collegeMajors = await page.evaluate( () =>{ // * Returns an array of all CSULB majors
+    return Array.from(document.querySelectorAll(".indexList ul li")).map(name => name.textContent)
+  })
+  
+  collegeMajors = [...new Set(collegeMajors)] // * Removes duplicates from array
+
+  collegeMajors = collegeMajors.map(majorName => ({major:majorName,isSelected:false}))
+
+
+  res.json(collegeMajors) // * Returns array of objects
+
+  await browser.close()
+})
+
+
+
+
+
 
 app.listen(PORT, () => { // * Server listens to PORT
   console.log(`Server listening on ${PORT}`);
